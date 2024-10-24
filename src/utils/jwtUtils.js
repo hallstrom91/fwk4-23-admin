529dd990eb4dc69f3b10eff8df9438dbbcd7a78d
hallstrom91 <simonhallstrom@live.se>
2024-10-24 11:23:50 +0200

    #27 - VerifyJWT utils / middleware to verify user before accessing GDPR endpoints

---
+++ src/utils/jwtUtils.js
@@ -0,0 +1,38 @@
+const fetch = require("node-fetch");
+
+const verifyJwt = async (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).json({ message: "Token missing" });
+  }
+
+  try {
+    const response = await fetch("http://localhost:3000/auth/verifyjwt", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: token.replace("Bearer ", ""),
+      }),
+    });
+
+    if (!response.ok) {
+      return res.status(401).json({ message: "Token verification failed" });
+    }
+
+    const data = await response.json();
+
+    if (data.verified) {
+      req.user = data.message;
+      next();
+    } else {
+      return res.status(401).json({ message: "Token verification failed" });
+    }
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = { verifyJwt };
