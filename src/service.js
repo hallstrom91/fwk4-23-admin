6ab466b5dab6f255b03397557f8c9b5795a878f1
Simon Hällström <simonhallstrom@live.se>
2024-10-31 14:07:38 +0100

    use cookie-parser

--- src/service.js
+++ src/service.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const gdprRoutes = require("./routes/gdprRoutes.js");
+const csrfRoutes = require("./routes/csrfRoutes.js");
+const PDFDocument = require("pdfkit");
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(helmet());
 app.use(cors());
 
 app.use("/gdpr", gdprRoutes);
+app.use("/csrf", csrfRoutes);
 
 module.exports = app;
