563cb060ccf28f65e6f65103e3ceb4d11e28fd1f
Simon Hällström <simonhallstrom@live.se>
2024-10-31 14:06:03 +0100

    #30 - update of csrf function, changed to cookie

--- src/utils/csrfUtils.js
+++ src/utils/csrfUtils.js
@@ -3,18 +3,22 @@ const crypto = require("crypto");
 const generateCsrfToken = (req, res) => {
   const csrfToken = crypto.randomBytes(32).toString("hex");
   res.app.locals.csrfToken = csrfToken;
-  return res.json({ csrfToken });
+  res.cookie("csrfToken", csrfToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    httpOnly: false, // true in https mode
+    sameSite: "lax",
+  });
+  return res.json({ message: "CSRF token set in cookies" });
 };
 
 const verifyCsrfToken = (req, res, next) => {
-  const csrfToken = req.headers["x-csrf-token"];
-  console.log("csrfToken", csrfToken);
-  if (csrfToken && csrfToken === req.app.locals.csrfToken) {
+  const csrfToken = req.cookies.csrfToken;
+  if (csrfToken === req.app.locals.csrfToken) {
     delete req.app.locals.csrfToken;
     return next();
   }
 
-  return res.status(403).json({ message: "Invalid or missing CSRF token" });
+  return res.status(403).json({ error: "Invalid or missing CSRF token" });
 };
 
 module.exports = { generateCsrfToken, verifyCsrfToken };
